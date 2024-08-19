85e9b38488a913664167f5400b2e99a338b906af
Fernando Frances <fernando95frances@gmail.com>
2024-08-19 14:44:42 +0200

    Segments in fractianl arch working

--- RecoveryArch/ContentView.swift
+++ RecoveryArch/ContentView.swift
@@ -37,16 +37,48 @@ struct FractionalArchView: View {
                         path.addArc(center: CGPoint(x: centerX, y: centerY),
                                     radius: radius,
                                     startAngle: .degrees(180 + 67.5),
-                                    endAngle: .degrees(360 - 67.5),
+                                    endAngle: .degrees(180 + 67.5 + 14.5),
                                     clockwise: false)
                     }
-                    .stroke(Color.gray, lineWidth: 4)
+                    .stroke(Color.hex(0xFF9500), style: .init(lineWidth: 4))
+                    
+                    Path { path in
+                        let width = geometry.size.width
+                        let height = geometry.size.height
+                        let centerX = width / 2
+                        let centerY = height
+                        let radius = width / 2
+                        path.addArc(center: CGPoint(x: centerX, y: centerY),
+                                    radius: radius,
+                                    startAngle: .degrees(180 + 67.5 + 15),
+                                    endAngle: .degrees(180 + 67.5 + 15 + 15),
+                                    clockwise: false)
+                    }
+                    .stroke(Color.blue, style: .init(lineWidth: 4))
+                    
+                    Path { path in
+                        let width = geometry.size.width
+                        let height = geometry.size.height
+                        let centerX = width / 2
+                        let centerY = height
+                        let radius = width / 2
+                        path.addArc(center: CGPoint(x: centerX, y: centerY),
+                                    radius: radius,
+                                    startAngle: .degrees(180 + 67.5 + 15 + 15.5),
+                                    endAngle: .degrees(180 + 67.5 + 15 + 15 + 15),
+                                    clockwise: false)
+                    }
+                    .stroke(Color.blue, style: .init(lineWidth: 4))
                     
                     // Draw the ball
                     Circle()
-                        .fill(Color.blue)
-                        .frame(width: 20, height: 20)
+                        .fill(Color.white)
+                        .overlay(
+                            Circle().stroke(lineWidth: 1).foregroundStyle(Color.black)
+                        )
+                        .frame(width: 12, height: 12)
                         .position(self.ballPosition(in: geometry.size))
+                        
                 }
             }
             .frame(height: 200)
@@ -54,6 +86,7 @@ struct FractionalArchView: View {
             // Slider to change the value
             Slider(value: $value, in: 0...100)
                 .padding()
+            
         }
     }
     
@@ -76,3 +109,18 @@ struct FractionalArchView: View {
     }
 }
 
+
+
+extension Color {
+    public static func hex(
+        _ hex: UInt,
+        opacity: Double = 1
+    ) -> Self {
+        Self(
+            red: Double((hex & 0xff0000) >> 16) / 255,
+            green: Double((hex & 0x00ff00) >> 8) / 255,
+            blue: Double(hex & 0x0000ff) / 255,
+            opacity: opacity
+        )
+    }
+}
